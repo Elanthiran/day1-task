7098cdef481d429f1aa683611c721f463ccc7539
Elanthiran <elanthirank@gmail.com>
2024-05-24 17:46:36 +0530

    updated

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("node:fs/promises")
+
+let date1=new Date()
+
+let date = date1.getDate();
+
+let month = date1.getMonth()+1;
+
+let year = date1.getFullYear()
+
+ const hours=date1.getHours()
+ const mins=  date1.getMinutes()
+  const secs= date1.getSeconds()
+
+const timeStamp=year+"-"+month+"-"+date+"-"+hours+":"+mins+":"+secs
+const date_time=date+"-"+hours
+function createFile(){
+
+fs.writeFile(`./date-time/${date_time}.txt`,`${timeStamp}`,"utf8")
+.then((error)=>
+{
+    if(error) 
+        {console.log(error)}
+        else {console.log("File is created")}
+})
+.catch((error)=>
+{
+    console.log(error)
+})
+}
+createFile();
+
+ function readDirectory(){
+    fs.readdir("./date-time")
+    .then((response)=>console.log(response))
+    .catch((error)=>console.log(error));
+}
+
+readDirectory();
